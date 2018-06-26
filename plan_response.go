334befac03f4d61a1c454711b988e30a3dd3bce1
Muflih Kholidin <muflic.24@gmail.com>
2018-06-27 03:18:47 +0700

    Add response plan

---
+++ plan_response.go
@@ -0,0 +1,16 @@
+package httplib
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func ResponsePlan(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(
+		data,
+	)
+	return
+}
