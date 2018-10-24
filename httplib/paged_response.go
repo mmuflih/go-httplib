7e9c61405e11dc49ff9e84e93492a1b81558af17
Okto Prima Jaya <octoprima93@gmail.com>
2018-10-24 16:35:25 +0700

    Update response paged

--- httplib/paged_response.go
+++ httplib/paged_response.go
@@ -6,10 +6,11 @@ import (
 )
 
 type DataPaginator struct {
-	Data  interface{} `json:"items"`
+	Data  interface{} `json:"data"`
 	Count int         `json:"total"`
 	Page  int         `json:"page"`
 	Size  int         `json:"size"`
+	Code  int         `json:"code"`
 }
 
 type pageResponse struct {
@@ -18,13 +19,9 @@ type pageResponse struct {
 }
 
 func ResponsePaged(w http.ResponseWriter, data DataPaginator) {
-	resp := pageResponse{
-		data,
-		http.StatusOK,
-	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	json.NewEncoder(w).Encode(resp)
+	data.Code = http.StatusOK
+	json.NewEncoder(w).Encode(data)
 	return
 }
